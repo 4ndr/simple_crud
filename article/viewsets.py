4c24445ec8410933c1dc4bd0238435c5c98a6ad6
andre <andrepereiraleite@gmail.com>
2019-04-13 15:29:32 -0300

    Working crud
    Signed-off-by: andre <andrepereiraleite@gmail.com>

---
+++ article/viewsets.py
@@ -0,0 +1,8 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import Article
+from .serializers import ArticleSerializer
+
+
+class ArticleViewSet(ModelViewSet):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
