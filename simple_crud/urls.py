fea3704b578fdedf662b692ba75e3e9ccc706ad3
andre <andrepereiraleite@gmail.com>
2019-04-13 15:23:51 -0300

    Working crud
    Signed-off-by: andre <andrepereiraleite@gmail.com>

--- simple_crud/urls.py
+++ simple_crud/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.views.generic import TemplateView
+
+from .routers import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('article/', TemplateView.as_view(template_name='index.html'))
 ]
